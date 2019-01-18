862a06e5aba1ecbcd6abdf081a501317d100fe44
EttOrm <40430949+EttOrm@users.noreply.github.com>
2019-01-18 20:47:38 +0100

    Update MusicRoom.cs

--- Source/Game/Scenes/MusicRoom.cs
+++ Source/Game/Scenes/MusicRoom.cs
@@ -17,7 +17,17 @@ namespace LifeDeath.Scenes
 
         Font font;
 
+        Entity camera;
         
+        Random random;
+
+        Entity lycoris;
+        Entity sprite2;
+        Entity sprite3;
+
+        Entity bCContainer1;
+        Entity bCContainer2;
+        Entity bCContainer3;
 
         // Create a reference set of entities and load necessary assets.
         public override void Preload(Entity root)
@@ -25,14 +35,69 @@ namespace LifeDeath.Scenes
             this.root = root;
 
             font = GlobalServices.GlobalFonts.Register("lifedeath:sans");
+
+
+            Sprite lycorisSprite = GlobalServices.GlobalSprites.Register("lifedeath:redlily");
+
+            lycoris = new Entity()
+                        .AddRenderComponent(new SpriteComponent(lycorisSprite));
+
+            sprite2 = new Entity()
+                        .AddRenderComponent(new SpriteComponent(lycorisSprite));
+
+            sprite3 = new Entity()
+                        .AddRenderComponent(new SpriteComponent(lycorisSprite));
+
+        }
+
+        public void Background(int bc)
+        {
+            switch (bc)
+            {
+                case 0:
+                    bCContainer1.SetVisible(false);
+                    bCContainer2.SetVisible(false);
+                    bCContainer3.SetVisible(false);
+                    break;
+
+                case 1:
+                    bCContainer1.SetVisible(true);
+                    bCContainer2.SetVisible(false);
+                    bCContainer3.SetVisible(false);
+                    break;
+
+                case 2:
+                    bCContainer1.SetVisible(false);
+                    bCContainer2.SetVisible(true);
+                    bCContainer3.SetVisible(false);
+                    break;
+
+                case 3:
+                    bCContainer1.SetVisible(false);
+                    bCContainer2.SetVisible(false);
+                    bCContainer3.SetVisible(true);
+                    break;
+            }
         }
 
         // Create the scene's entities by cloning reference entities.
         public override void Load()
         {
+            container = new Entity()
+                .AttachTo(root);
 
-            container = new Entity();
-            container.AttachTo(root);
+            camera = new Entity()
+                .SetRenderByDepth(true)
+                .AttachTo(container);
+
+            bCContainer1 = new Entity()
+                .AttachTo(camera);
+
+            bCContainer2 = new Entity()
+                .AttachTo(camera);
+
+            bCContainer3 = new Entity()
+                .AttachTo(camera);
 
             new Entity(0, 0)
                 .AddRenderComponent(new TextComponent(font, "Music Room"))
@@ -46,6 +111,45 @@ namespace LifeDeath.Scenes
                .AddRenderComponent(new TextComponent(font, "Press Esc To Return To The Main Menu"))
                .AddUpdateComponent(new Components.MenuComponent())
                .AttachTo(container);
+
+
+            random = new Random();
+            double phi = (Math.Sqrt(5d) - 1d) / 2d;
+            double theta = random.NextDouble();
+            for (int i = 0; i < 200; i++)
+            {
+                lycoris.Clone()
+                    //.SetPosition(random.Next(0, 700), random.Next(0, 700))
+                    //.AddChainComponent("motion", new Components.WindyComponent(random.Next(0, 1280), random.Next(0, 720)))
+                    .AddChainComponent("motion", new Components.WindyComponent(null, camera, (float)(theta * 1280d + random.NextDouble() * 200d - 100d), (float)i * 920f / 200f))
+                    .AttachTo(bCContainer1);
+                theta += phi;
+                if (theta > 1d) theta -= 1d;
+            }
+
+            for (int i = 0; i < 200; i++)
+            {
+                sprite2.Clone()
+                    //.SetPosition(random.Next(0, 700), random.Next(0, 700))
+                    //.AddChainComponent("motion", new Components.WindyComponent(random.Next(0, 1280), random.Next(0, 720)))
+                    .AddChainComponent("motion", new Components.WindyComponent(null, camera, (float)(theta * 1280d + random.NextDouble() * 200d - 100d), (float)i * 920f / 200f))
+                    .AttachTo(bCContainer2);
+                theta += phi;
+                if (theta > 1d) theta -= 1d;
+            }
+
+            for (int i = 0; i < 200; i++)
+            {
+                sprite3.Clone()
+                    //.SetPosition(random.Next(0, 700), random.Next(0, 700))
+                    //.AddChainComponent("motion", new Components.WindyComponent(random.Next(0, 1280), random.Next(0, 720)))
+                    .AddChainComponent("motion", new Components.WindyComponent(null, camera, (float)(theta * 1280d + random.NextDouble() * 200d - 100d), (float)i * 920f / 200f))
+                    .AttachTo(bCContainer3);
+                theta += phi;
+                if (theta > 1d) theta -= 1d;
+            }
+
+
         }
 
         // Delete the scene. (Reference entities and assets remain.)
